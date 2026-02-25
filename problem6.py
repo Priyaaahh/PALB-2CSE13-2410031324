63e340644d6fbbb0ea5a6663644906feeb9be982
Priyaaahh <priyakri.1664@gmail.com>
2026-02-26 00:36:07 +0530

    Add files via upload

---
+++ problem6.py
@@ -0,0 +1,11 @@
+def rotate_by_one(arr):
+    last = arr[-1]
+    for i in range(len(arr) - 1, 0, -1):
+        arr[i] = arr[i - 1]
+    arr[0] = last
+    return arr
+
+
+# Example usage
+arr = [1, 2, 3, 4, 5]
+print(rotate_by_one(arr))
