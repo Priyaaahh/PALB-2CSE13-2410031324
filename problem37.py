2d4960ae90e40508ba0bc8a061719d462f02f734
Priyaaahh <priyakri.1664@gmail.com>
2026-02-26 12:12:11 +0530

    Update problem37.py

--- problem37.py
+++ problem37.py
@@ -1,28 +1,61 @@
-def sortColors(nums):
-    low = 0
-    mid = 0
-    high = len(nums) - 1
+def exist(board, word):
+    rows = len(board)
+    cols = len(board[0])
 
-    while mid <= high:
-        if nums[mid] == 0:
-            nums[low], nums[mid] = nums[mid], nums[low]
-            low += 1
-            mid += 1
+    def dfs(r, c, index):
+        if index == len(word):
+            return True
 
-        elif nums[mid] == 1:
-            mid += 1
+        if (r < 0 or c < 0 or 
+            r >= rows or c >= cols or 
+            board[r][c] != word[index]):
+            return False
 
-        else: 
-            nums[mid], nums[high] = nums[high], nums[mid]
-            high -= 1
+        temp = board[r][c]
+        board[r][c] = "#"
+
+        found = (dfs(r+1, c, index+1) or
+                 dfs(r-1, c, index+1) or
+                 dfs(r, c+1, index+1) or
+                 dfs(r, c-1, index+1))
+
+        board[r][c] = temp
+
+        return found
+
+    for i in range(rows):
+        for j in range(cols):
+            if dfs(i, j, 0):
+                return True
+
+    return False
 
 
 #Example1
-nums1 = [2,0,2,1,1,0]
-sortColors(nums1)
-print("Sorted array 1:", nums1)
+board1 = [
+    ["A","B","C","E"],
+    ["S","F","C","S"],
+    ["A","D","E","E"]
+]
+word1 = "ABCCED"
+print("Example 1:", exist(board1, word1))  
+
 
 #Example2
-nums2 = [2,0,1]
-sortColors(nums2)
-print("Sorted array 2:", nums2)
+board2 = [
+    ["A","B","C","E"],
+    ["S","F","C","S"],
+    ["A","D","E","E"]
+]
+word2 = "SEE"
+print("Example 2:", exist(board2, word2))  
+
+
+# Example3
+board3 = [
+    ["A","B","C","E"],
+    ["S","F","C","S"],
+    ["A","D","E","E"]
+]
+word3 = "ABCB"
+print("Example 3:", exist(board3, word3))  
