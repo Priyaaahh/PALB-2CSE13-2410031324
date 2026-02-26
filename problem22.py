ad79aff4f00507809282a5ee94a88141f362bebd
Priyaaahh <priyakri.1664@gmail.com>
2026-02-26 10:36:34 +0530

    Add files via upload

---
+++ problem22.py
@@ -0,0 +1,20 @@
+def smallest_subarray_with_sum(x, arr):
+    n = len(arr)
+    min_len = float('inf')
+    curr_sum = 0
+    start = 0
+
+    for end in range(n):
+        curr_sum += arr[end]
+
+        while curr_sum > x:
+            min_len = min(min_len, end - start + 1)
+            curr_sum -= arr[start]
+            start += 1
+
+    return 0 if min_len == float('inf') else min_len
+
+
+# Example 
+print(smallest_subarray_with_sum(51, [1, 4, 45, 6, 0, 19]))  
+print(smallest_subarray_with_sum(100, [1, 10, 5, 2, 7]))    
