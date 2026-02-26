b3e3c38d0376bd4694ad3bd99cca7a89b5731f2d
Priyaaahh <priyakri.1664@gmail.com>
2026-02-26 10:23:23 +0530

    Add files via upload

---
+++ problem19.py
@@ -0,0 +1,24 @@
+def triplet_sum_exists(arr, target):
+    n = len(arr)
+    arr.sort()
+
+    for i in range(n - 2):
+        left = i + 1
+        right = n - 1
+
+        while left < right:
+            curr_sum = arr[i] + arr[left] + arr[right]
+            if curr_sum == target:
+                return True
+            elif curr_sum < target:
+                left += 1
+            else:
+                right -= 1
+
+    return False
+
+
+# Example 
+print(triplet_sum_exists([1, 4, 45, 6, 10, 8], 13))   
+print(triplet_sum_exists([1, 2, 4, 3, 6, 7], 10))    
+print(triplet_sum_exists([40, 20, 10, 3, 6, 7], 24)) 
