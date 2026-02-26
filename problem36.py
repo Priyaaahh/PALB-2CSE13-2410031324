52c435cd139df686f486da99b6a02e455508eb1a
Priyaaahh <priyakri.1664@gmail.com>
2026-02-26 12:07:34 +0530

    Update problem36.py

--- problem36.py
+++ problem36.py
@@ -1,42 +1,25 @@
-def searchMatrix(matrix, target):
-    if not matrix or not matrix[0]:
-        return False
+def subsets(nums):
+    result = []
 
-    m = len(matrix)
-    n = len(matrix[0])
+    def backtrack(start, current):
+        result.append(current[:])
 
-    left = 0
-    right = m * n - 1
+        for i in range(start, len(nums)):
+            current.append(nums[i])
+            
+            backtrack(i + 1, current)
+            current.pop()
 
-    while left <= right:
-        mid = (left + right) // 2
-
-
-        row = mid // n
-        col = mid % n
-
-        mid_value = matrix[row][col]
-
-        if mid_value == target:
-            return True
-        elif mid_value < target:
-            left = mid + 1
-        else:
-            right = mid - 1
-
-    return False
+    backtrack(0, [])
+    return result
 
 
-#Example
+# Example1
+nums1 = [1, 2, 3]
+print("Subsets of", nums1, ":")
+print(subsets(nums1))
 
-matrix = [
-    [1, 3, 5, 7],
-    [10, 11, 16, 20],
-    [23, 30, 34, 60]
-]
-
-target1 = 3
-target2 = 13
-
-print("Searching for", target1, ":", searchMatrix(matrix, target1))
-print("Searching for", target2, ":", searchMatrix(matrix, target2))
+# Example2
+nums2 = [0]
+print("\nSubsets of", nums2, ":")
+print(subsets(nums2))
